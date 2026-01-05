2fa10743c3b848e0da14859f791f1c23f8eba4d2
Waiharrington <64016167+Waiharrington@users.noreply.github.com>
2026-01-05 08:05:33 -0400

    Fix: Rewrite end of file with 2 inner 2 outer divs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1176,9 +1176,8 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            </div>
           ))}
+      </div>
     </div>
-    </div >
   )
 }
