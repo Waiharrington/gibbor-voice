61a2cbb5119dd2028ddf05df87da93e19346e46b
Waiharrington <64016167+Waiharrington@users.noreply.github.com>
2026-02-09 15:59:57 -0400

    HOTFIX: Fix syntax error in /token endpoint (Restore AccessToken constructor)

--- backend/server.js
+++ backend/server.js
@@ -55,9 +55,12 @@ app.get("/token", async (req, res) => {
             incomingAllow: true
         });
 
-        process.env.TWILIO_API_KEY,
+        const token = new AccessToken(
+            process.env.TWILIO_ACCOUNT_SID,
+            process.env.TWILIO_API_KEY,
             process.env.TWILIO_API_SECRET,
             { identity: identity, ttl: 86400 } // 24 Hour Token TTL
+        );
 
         token.addGrant(voiceGrant);
 
